1ecb2cf97c89596c4c57cda527e5b4f22a1cf985
Diallo Abdoulaye <djibi26072018@gmail.com>
2026-05-26 19:48:00 -0400

    readded resources folder

--- gentech_fintech/src/main/java/com/abdoul/gentech_fintech/Services/UserService.java
+++ gentech_fintech/src/main/java/com/abdoul/gentech_fintech/Services/UserService.java
@@ -4,7 +4,10 @@ import com.abdoul.gentech_fintech.DTO.UserDTO;
 import com.abdoul.gentech_fintech.Exceptions.BadRequestException;
 import com.abdoul.gentech_fintech.Exceptions.ConflictException;
 import com.abdoul.gentech_fintech.Models.UserModel;
+import com.abdoul.gentech_fintech.Models.WalletModel;
 import com.abdoul.gentech_fintech.Repositories.UserRepository;
+import com.abdoul.gentech_fintech.Repositories.WalletRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -12,9 +15,13 @@ import java.util.Map;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder encoder;
+    private final WalletRepository walletRepository;
 
-    public UserService (UserRepository userRepository){
+    public UserService (UserRepository userRepository, BCryptPasswordEncoder encoder, WalletRepository walletRepository){
         this.userRepository = userRepository;
+        this.encoder = encoder;
+        this.walletRepository = walletRepository;
     }
 
     public Map<String, String> createAccount(UserDTO.SignUp data){
@@ -27,6 +34,19 @@ public class UserService {
         if (existingUser != null && !existingUser.isDeleted()){
             throw new ConflictException("Account already exists");
         }
+
+        UserModel newUser = new UserModel();
+        newUser.setName(data.getName());
+        newUser.setEmail(data.getEmail());
+        newUser.setPassword(encoder.encode(data.getPassword()));
+        newUser.setPhone(data.getPhone());
+
+        userRepository.saveAndFlush(newUser);
+
+        WalletModel newWallet = new WalletModel();
+        newWallet.setUser(newUser);
+
+        walletRepository.save(newWallet);
 
 
     }
