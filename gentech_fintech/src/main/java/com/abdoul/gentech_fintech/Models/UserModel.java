8fe8f56c227da14f96249fd5c06e2a7a7ec75878
Diallo Abdoulaye <djibi26072018@gmail.com>
2026-05-26 17:40:15 -0400

    Added WalletModel entity with lombtok

--- gentech_fintech/src/main/java/com/abdoul/gentech_fintech/Models/UserModel.java
+++ gentech_fintech/src/main/java/com/abdoul/gentech_fintech/Models/UserModel.java
@@ -1,12 +1,21 @@
 package com.abdoul.gentech_fintech.Models;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 @Entity
 @Table(name = "users", indexes ={
         @Index(columnList = "email"),
         @Index(columnList = "phone")
 })
+@Getter
+@Setter
+@NoArgsConstructor
 public class UserModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,12 +26,32 @@ public class UserModel {
 
     private String phone;
 
+    @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private boolean deleted = false;
 
+    @Column(nullable = false)
     private boolean flagged = false;
+
+    @Column(columnDefinition = "TIMESTAMP WITH TIME ZONE")
+    private ZonedDateTime createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITH TIME ZONE")
+    private ZonedDateTime updatedAt;
+
+    @PrePersist
+    private void onCreate (){
+        this.createdAt = ZonedDateTime.now(ZoneId.of("UTC"));
+    }
+
+    @PreUpdate
+    private void onUpdate(){
+        this.updatedAt = ZonedDateTime.now(ZoneId.of("UTC"));
+    }
 
 }
--- gentech_fintech/src/main/java/com/abdoul/gentech_fintech/Models/WalletModel.java
+++ gentech_fintech/src/main/java/com/abdoul/gentech_fintech/Models/WalletModel.java
@@ -1,4 +1,39 @@
 package com.abdoul.gentech_fintech.Models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "wallets", indexes = {
+        @Index(columnList = "wallet_user")
+})
+@Getter
+@Setter
+@NoArgsConstructor
 public class WalletModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "wallet_id")
+    private Long id;
+
+    @Column(precision = 29, scale = 2, nullable = false)
+    private BigDecimal balance = BigDecimal.ZERO;
+
+    @OneToOne
+    @JoinColumn(name = "wallet_user", nullable = false)
+    private UserModel user;
+
+    @Column(columnDefinition = "TIMESTAMP WITH TIME ZONE", nullable = false)
+    private ZonedDateTime createdAt;
+
+    @PrePersist
+    private void onCreate (){
+        this.createdAt = ZonedDateTime.now(ZoneId.of("UTC"));
+    }
 }
