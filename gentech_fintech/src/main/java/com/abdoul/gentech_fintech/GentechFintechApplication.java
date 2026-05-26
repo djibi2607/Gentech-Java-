a9464c0ea8d5e9fafd095aeb47ab126e5d724a8a
Diallo Abdoulaye <djibi26072018@gmail.com>
2026-05-26 15:28:59 -0400

    Initial commit

---
+++ gentech_fintech/src/main/java/com/abdoul/gentech_fintech/GentechFintechApplication.java
@@ -0,0 +1,13 @@
+package com.abdoul.gentech_fintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GentechFintechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GentechFintechApplication.class, args);
+	}
+
+}
---
+++ gentech_fintech/src/test/java/com/abdoul/gentech_fintech/GentechFintechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdoul.gentech_fintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GentechFintechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
