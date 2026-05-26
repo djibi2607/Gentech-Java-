cabbfc7968515c1d3a6c17c7a875c715ec9abfa3
Diallo Abdoulaye <djibi26072018@gmail.com>
2026-05-26 18:12:42 -0400

    added UserDTO with SignUp data validation

--- gentech_fintech/src/main/java/com/abdoul/gentech_fintech/Services/UserService.java
+++ gentech_fintech/src/main/java/com/abdoul/gentech_fintech/Services/UserService.java
@@ -2,7 +2,12 @@ package com.abdoul.gentech_fintech.Services;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 public class UserService {
 
+
+
+    public Map<String, String> createAccount()
 }
