51c3ab5ebbce2bc98a54f00c36bb5279c271c3ee
Diallo Abdoulaye <djibi26072018@gmail.com>
2026-05-29 15:23:43 -0400

    added TransactionModel to track transactions records

---
+++ gentech_fintech/src/main/java/com/abdoul/gentech_fintech/Models/TransactionModel.java
@@ -0,0 +1,47 @@
+package com.abdoul.gentech_fintech.Models;
+
+import com.abdoul.gentech_fintech.Configuration.TransType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "transactions")
+@Getter
+@Setter
+@NoArgsConstructor
+public class TransactionModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(precision = 7, scale = 2, nullable = false)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private TransType transType;
+
+    @Column(columnDefinition = "TIMESTAMP WITH TIME ZONE")
+    private ZonedDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = ZonedDateTime.now(ZoneId.of("UTC"));
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "sender_wallet")
+    private WalletModel senderWallet;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_wallet")
+    private WalletModel receiverWallet;
+}
