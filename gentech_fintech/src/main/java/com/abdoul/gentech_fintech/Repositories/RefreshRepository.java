051aee4d4c83ece931e3efb93ddba46135736d77
Diallo Abdoulaye <djibi26072018@gmail.com>
2026-06-01 17:00:36 -0400

    added query to delete tokens by their expiration date

--- gentech_fintech/src/main/java/com/abdoul/gentech_fintech/Repositories/RefreshRepository.java
+++ gentech_fintech/src/main/java/com/abdoul/gentech_fintech/Repositories/RefreshRepository.java
@@ -4,6 +4,10 @@ import com.abdoul.gentech_fintech.Models.RefreshModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.ZonedDateTime;
+import java.util.List;
+
 @Repository
 public interface RefreshRepository extends JpaRepository<RefreshModel, Long> {
+    void deleteByExpiresAtBefore(ZonedDateTime expired);
 }
