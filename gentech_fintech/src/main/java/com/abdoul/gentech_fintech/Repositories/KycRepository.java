09618f2765a97c3847da403c94c544d2e58163ce
Diallo Abdoulaye <djibi26072018@gmail.com>
2026-06-01 17:28:27 -0400

    added query that finds users getting close to kyc deadline

--- gentech_fintech/src/main/java/com/abdoul/gentech_fintech/Repositories/KycRepository.java
+++ gentech_fintech/src/main/java/com/abdoul/gentech_fintech/Repositories/KycRepository.java
@@ -5,7 +5,14 @@ import com.abdoul.gentech_fintech.Configuration.KycType;
 import com.abdoul.gentech_fintech.Models.KycModel;
 import com.abdoul.gentech_fintech.Models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.ZonedDateTime;
+import java.util.List;
 
 public interface KycRepository extends JpaRepository<KycModel, Long> {
     KycModel findByUserAndKycType(UserModel user, KycType kycType);
+    @Query("SELECT k FROM KycModel k JOIN k.user u WHERE k.submittedAt < :tenDaysAgo AND k.status = :status AND k.kycRemainder = false AND u.deleted = false AND k.url IS NULL")
+    List<KycModel> findKyc(@Param("tenDaysAgo")ZonedDateTime tenDayAgo, @Param("status")KycStatus kycStatus);
 }
